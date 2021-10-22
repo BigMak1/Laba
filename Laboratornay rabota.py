f3ec0006020f4a3069b74a9d69b23a158e34f912
BigMak1 <90455456+BigMak1@users.noreply.github.com>
2021-10-22 14:52:52 +0300

    Update Laboratornay rabota.py

--- Laboratornay rabota.py
+++ Laboratornay rabota.py
@@ -1,63 +1,61 @@
-file1 = open('pokemon_full.json')
+with open('pokemon_full.json') as file:
 
 # Task1
 
-s = file1.read()  # делаем из файла строку
-print('№1')
-print(len(s))
+    string = file.read()  # Делаем из файла строку
+    chars_amount = len(string)
+    print(f'1)Общее количество символов: {chars_amount}')
 
 # Task 2
 
-kol1 = 0        # количество символов без пробелов и знаков препинания
-for i in s:
-    if 65 <= ord(i) <= 90 or 97 <= ord(i) <= 122 or 48 <= ord(i) <= 57:    # сравниваем по Юникоду
-        kol1 += 1
-print('№2')
-print(kol1)
+    chars_amount1 = 0        # Будем записывать количество символов без пробелов и знаков препинания
+    for count in string:
+        if count.isalnum():  # Находим символы, которые не являются знакоми препинания
+            chars_amount1 += 1
+    print(f'2)Общее количество символов без знаков препинания: {chars_amount1}')
 
 # Task 3
 
-file2 = open('pokemon_full.json')
-kol2 = 0          # количество символов в строке "description" текущего покемона
-name = ''         # имя текущего покемона
-maxkol2 = 0       # максимальное кол-во символов
-maxname = ''      # имя покемона с максимальным кол-вом
-while True:
-    line = file2.readline()  # читаем файл по строкам
-    if not line:
-        break
-    line = line.strip()      # убираем пробелы пред строкой
-    if "name" in line:
-        name = line
-    if "description" in line:
-        kol2 = len(line)
-        if kol2 > maxkol2:
-            maxkol2 = kol2
-            maxname = name
-maxname = maxname[9:-2:1]   # убираем лишние символы из строки
-print('№3')
-print(maxname)
+    file.seek(0)
+    sum = 0          # Будем считать количество символов в строке "description" текущего покемона
+    name = ''         # Будем записывать имя текущего покемона
+    max_sum = 0       # Будем хранить максимальное кол-во символов
+    max_name = ''      # Будем хранить имя покемона с максимальным кол-вом
+    while True:
+        line = file.readline()  # Читаем файл по строкам
+        if not line:
+            break
+        line = line.strip()      # Убираем пробелы пред строкой
+        if 'name' in line:
+            name = line
+        if 'description' in line:
+            sum = len(line)
+            if sum > max_sum:
+                max_sum = sum
+                max_name = name
+    max_name = max_name[9:-2:1]   # Убираем лишние символы из строки
+    print(f'3)Самое длинное описание у {max_name}')
 
 # Task 4
 
-file3 = open('pokemon_full.json')
-ind = 0            # индификатор того, что у нас перечисляются способности покемона
-prob = 0           # количество пробелов в строке со способностью
-maxprob = 0        # максимальное количество пробелов в строке, то есть максимальное кол-во слов
-maxabil = ''       # способность с максимальным кол-вом слов
-while True:
-    line1 = file3.readline()  # читаем файл по строкам
-    if not line1:
-        break
-    line1 = line1.strip()      # убираем пробелы пред строкой
-    if '],' in line1:
-        ind = 0
-    if ind == 1:
-        prob = line1.count(' ') - line1.count(' "')  # вычитаем пробелы, которые идут перед ковычками
-        if prob >= maxprob:
-            maxprob = prob
-            maxabil = line1
-    if 'abilities' in line1:
-        ind = 1
-print('№4')
-print(maxabil[1:-1])  # убираем кавычки из ответа
+    file.seek(0)
+    counter = 0            # Заведем индификато(счетчик) того, что у нас перечисляются способности покемона
+    space = 0           # Будем записывать количество пробелов в строке со способностью
+    max_space = 0        # Будем хранить максимальное количество пробелов в строке, то есть максимальное кол-во слов
+    max_ability = ''       # Будем хранить способность с максимальным кол-вом слов
+    while True:
+        line1 = file.readline()  # Читаем файл по строкам
+        if not line1:
+            break
+        line1 = line1.strip()      # Убираем пробелы пред строкой
+        if '],' in line1:
+            counter = 0
+        if counter == 1:
+            space = line1.count(' ') - line1.count(' "')  # Вычитаем пробелы, которые идут перед ковычками
+            if space >= max_space:
+                max_space = space
+                max_ability = line1
+        if 'abilities' in line1:
+            counter = 1
+    max_ability = max_ability[1:-1]  # Убираем кавычки из ответа
+    print(f'4)Умение, которое содержит больше всего слов - это {max_ability}')
